e8a2480df3ecdd6aba65c77991a15041156541c3
handbinu <canflerang@naver.com>
2023-07-11 13:30:01 +0900

    php 비연동 테스트

---
+++ HTML/loginProcess.php
@@ -0,0 +1,43 @@
+    <?php
+    $conn = mysqli_connect("127.0.0.1", "root", "pma5hapzizol", "daily-art", "3306");
+    $login_id = $_POST['userId'];
+    $password = $_POST['password'];
+
+    // DB 정보 가져오기 
+    $sql = "SELECT * FROM user WHERE userId ='{$login_id}'";
+    $result = mysqli_query($conn, $sql);
+
+    $row = mysqli_fetch_array($result);
+    $hashedPassword = $row['password'];
+    $row['Userid'];
+
+    foreach($row as $key => $r){
+        echo "{$key} : {$r} <br>";
+    }
+    // echo $row['id'];
+    // DB 정보를 가져왔으니 
+    // 비밀번호 검증 로직을 실행하면 된다.
+    $passwordResult = password_verify($password, $hashedPassword);
+    if ($passwordResult === true) {
+        // 로그인 성공
+        // 세션에 id 저장
+        session_start();
+        $_SESSION['userId'] = $row['Userid'];
+        print_r($_SESSION);
+        echo $_SESSION['userId'];
+    
+?>
+    <script>
+        alert("로그인에 성공하였습니다.")
+        location.href = "main.php";
+    </script>
+<?php
+} else {
+    // 로그인 실패 
+?>
+    <script>
+        alert("로그인에 실패하였습니다");
+    </script>
+<?php
+}
+?>
