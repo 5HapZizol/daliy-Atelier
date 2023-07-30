b086b52953c62090c9a231b1d8f00412edc86814
seoha6895 <signis1010@naver.com>
2023-07-30 18:46:04 +0900

    경로 수정하고 헤더랑 input 겹치는거 수정함 + 무한 스크롤 구현했씁니다 ^_^

--- js/endlessScroll.js
+++ js/endlessScroll.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
     if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         var toAdd = document.createElement("div");
         toAdd.classList.add("box_1")
-        toAdd.textContent = `${++count}번째 블록`
+        //toAdd.textContent = `${++count}번째 블록`
         document.querySelector('section').appendChild(toAdd);
     }
 }
