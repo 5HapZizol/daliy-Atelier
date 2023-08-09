29f7386e68cdebdac994e40d87e8673acc1026d5
공수레공수거\user <wpdsls101@gmail.com>
2023-08-09 20:33:41 +0900

    전채영입니다. 어.. 주문목록 페이지 디자인 완성해서 올리구요.. 헤더랑 푸터는 복붙하기 귀찮아서 따로 html로 만들고 js파일로 붙였는데 푸터는 모양 이상해져서 그냥 복붙해놨어요..

---
+++ js/Order_status.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".Header").load("../html/Header.html");
+});
+
+// $(document).ready(function(){
+//     $(".footer-Background").load("../html/Footer.html");
+// });
