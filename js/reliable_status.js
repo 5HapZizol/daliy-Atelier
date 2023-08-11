a9f71b20dcf6bac0e55b2c0f7b695ebd0a321ecc
공수레공수거\user <wpdsls101@gmail.com>
2023-08-11 17:28:11 +0900

    주인님~~ 제가 진짜에요!!!!!!!! 이게 ㄹㅇ 최신이다 제발 풀해라

---
+++ js/reliable_status.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".Header").load("../html/Header.html");
+});
+
+$(document).ready(function(){
+  $(".footer-Background").load("../html/Footer.html");
+});
+
+$(document).ready(function() {
+    $('.Tap-Choice div').on('click', function() {
+      const target = this;
+      $('.Tap-Choice div').removeClass('active');
+      $(target).addClass('active');
+    });
+
+    // 페이지 로드 시 첫 번째 자식 요소인 "Bid status"를 기본으로 설정
+    $('.Tap-Choice div:first-child').addClass('active');
+  });
