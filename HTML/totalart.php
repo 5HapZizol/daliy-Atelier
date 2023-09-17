e7f47012a31de6dcc5bd8cb156f849dd5b54f1df
공수레공수거\user <wpdsls101@gmail.com>
2023-09-17 21:51:57 +0900

    Merge branch 'sonbinu' of https://github.com/5HapZizol/daliy-Atelier into Jeon
    # Conflicts:
    #	HTML/totalart.php

--- HTML/totalart.php
+++ HTML/totalart.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< HEAD
      $conn = mysqli_connect("127.0.0.1", "root", "0430!!", "daily-art", "3306");
+=======
+     $conn = mysqli_connect("127.0.0.1", "root", "pma5hapzizol", "daily-art", "3306");
+>>>>>>> 36fdcc93b0a15f3335f06b55e3c7cf7d66c52535
      //$conn = mysqli_connect(주소, 아이디, "비밀번호", DB 스키마 이름, 포트);
    
      //if($pageNumber < 1) $pageNumber = 1;
