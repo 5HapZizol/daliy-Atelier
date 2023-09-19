e3fa24117fea0e9a9d0ff77d95331cce0f9d6c85
seoha6895 <signis1010@naver.com>
2023-09-19 19:50:25 +0900

    작가 회원가입 header 사라진거 추가했어요

--- HTML/Q_list.php
+++ HTML/Q_list.php
@@ -14,6 +14,7 @@
 
       
   <article>
+  <div style="margin-top: 200px;">
     <div>
 
       <div class="page_top">
@@ -27,82 +28,81 @@
         <button class="btn_Q" onclick="location.href='Q_write.html'">문의하기</button>
     </div>
 
-    <table align="center">
-        <thead class="tb_head">
-            <tr>
-                <th>번호</th> <th>구분</th> <th>제목</th> <th>작성자 ID</th> <th>작성일</th> <th>상태</th>
-            </tr>    
-        </thead>
+    <div class = "container">
+        <table class = "Q_list-table">
+            <thead class="tb_head">
+                <tr>
+                    <th>번호</th> <th>구분</th> <th>제목</th> <th>작성자 ID</th> <th>작성일</th> <th>상태</th>
+                </tr>    
+            </thead>
 
-        <tbody class="tb_body">
-            <div class="safe">
-                <tr >
-                    <td>8203</td> <td>배송</td> <td>배송이 안와요</td> <td>se***34</td> <td>23-03-12</td> <td>대기</td>
-                </tr>
-                <tr>
-                    <td>8204</td> <td>포인트</td> <td>포인트 적립 환불</td> <td>du***94</td> <td>23-03-12</td> <td>대기</td>
-                </tr>
-                <tr >
-                    <td>8203</td> <td>배송</td> <td>배송이 안와요</td> <td>se***34</td> <td>23-03-12</td> <td>대기</td>
-                </tr>
-                <tr>
-                    <td>8204</td> <td>포인트</td> <td>포인트 적립 환불</td> <td>du***94</td> <td>23-03-12</td> <td>대기</td>
-                </tr>
-                <tr>    
-                    <td>8205</td> <td>회원정보</td> <td>일반회원에서 작가회원으로 변경하고 싶습니다</td> <td>as***57</td> <td>23-03-12</td> <td>대기</td>
-                </tr>
-                <tr>    
-                    <td>8205</td> <td>회원정보</td> <td>일반회원에서 작가회원으로 변경하고 싶습니다</td> <td>as***57</td> <td>23-03-12</td> <td>대기</td>
-                </tr>
-            </div>
+            <tbody class="tb_body">
+                    <tr >
+                        <td>8203</td> <td>배송</td> <td>배송이 안와요</td> <td>se***34</td> <td>23-03-12</td> <td>대기</td>
+                    </tr>
+                    <tr>
+                        <td>8204</td> <td>포인트</td> <td>포인트 적립 환불</td> <td>du***94</td> <td>23-03-12</td> <td>대기</td>
+                    </tr>
+                    <tr >
+                        <td>8203</td> <td>배송</td> <td>배송이 안와요</td> <td>se***34</td> <td>23-03-12</td> <td>대기</td>
+                    </tr>
+                    <tr>
+                        <td>8204</td> <td>포인트</td> <td>포인트 적립 환불</td> <td>du***94</td> <td>23-03-12</td> <td>대기</td>
+                    </tr>
+                    <tr>    
+                        <td>8205</td> <td>회원정보</td> <td>일반회원에서 작가회원으로 변경하고 싶습니다</td> <td>as***57</td> <td>23-03-12</td> <td>대기</td>
+                    </tr>
+                    <tr>    
+                        <td>8205</td> <td>회원정보</td> <td>일반회원에서 작가회원으로 변경하고 싶습니다</td> <td>as***57</td> <td>23-03-12</td> <td>대기</td>
+                    </tr>
+            </tbody>
 
-            <div class="done">
-                <tr>    
-                    <td>8206</td> <td>작품</td> <td>디지털아트와 실물아트의 차이점은 뭔가요?</td> <td>qw***99</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>
-                    <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>   
-                <tr>    
-                    <td>8206</td> <td>작품</td> <td>디지털아트와 실물아트의 차이점은 뭔가요?</td> <td>qw***99</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>
-                    <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>
-                    <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8206</td> <td>작품</td> <td>디지털아트와 실물아트의 차이점은 뭔가요?</td> <td>qw***99</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>
-                    <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>
-                    <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-                <tr>    
-                    <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
-                </tr>
-            </div>
-        </tbody>
-    </table>
-
+                    <tr>    
+                        <td>8206</td> <td>작품</td> <td>디지털아트와 실물아트의 차이점은 뭔가요?</td> <td>qw***99</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>
+                        <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>   
+                    <tr>    
+                        <td>8206</td> <td>작품</td> <td>디지털아트와 실물아트의 차이점은 뭔가요?</td> <td>qw***99</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>
+                        <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>
+                        <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8206</td> <td>작품</td> <td>디지털아트와 실물아트의 차이점은 뭔가요?</td> <td>qw***99</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>
+                        <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>
+                        <td>8208</td> <td>배송</td> <td>배송은 보통 며칠 걸리나요?</td> <td>fn***26</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+                    <tr>    
+                        <td>8207</td> <td>포인트</td> <td>최소얼마부터 충정가능 한가요?</td> <td>wg***14</td> <td>23-03-11</td> <td class="Q_stay">완료</td>
+                    </tr>
+            </tbody>
+        </table>
+    </div>
+    
     <div style = "margin-top: 50px;"></div>
     <div class = "ques">
       <div class="btn-group">
