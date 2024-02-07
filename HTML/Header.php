54a851588228da2d3faa98baadc318bef6b31560
DESKTOP-C6DR449\sonyebin <canflerang@naver.com>
2024-02-08 01:59:04 +0900

    헤더 css 깨짐 오류 수정

--- HTML/Header.php
+++ HTML/Header.php
@@ -8,8 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/header.css">
     <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <link rel="stylesheet" href="../css/header.css">
     <script src="../js/header.js"></script>
     <title>헤더</title>
     <script>
