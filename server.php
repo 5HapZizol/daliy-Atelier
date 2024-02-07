73e724a084fb5a0be9cfc38433f16417cbf7e90b
DESKTOP-C6DR449\sonyebin <canflerang@naver.com>
2024-02-08 01:14:28 +0900

    배포를 위한 환경변수 서버 접속

--- server.php
+++ server.php
@@ -1,7 +1,11 @@
 <?php
     date_default_timezone_set('Asia/Seoul');
     ini_set('display_errors','0');
+    $host = getenv('HOST');
+    $id = getenv('DB_ID');
+    $port = getenv('DB_PORT');
+    $pw = getenv('DB_PW');
 
-    $conn = mysqli_connect("127.0.0.1", "root", "pma002244", "daily-art", "3306");
+    $conn = mysqli_connect($host, $id, $pw, "daily-art", $port);
     //$conn = mysqli_connect(주소, 아이디, "비밀번호", DB 스키마 이름, 포트);
 ?>
