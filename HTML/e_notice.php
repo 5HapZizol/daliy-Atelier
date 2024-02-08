5f72b0ef56aebfa808fb2af25634a8e1fe521c50
DESKTOP-C6DR449\sonyebin <canflerang@naver.com>
2024-02-08 12:04:03 +0900

    공지사항 링크 수정

--- HTML/e_notice.php
+++ HTML/e_notice.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="..\CSS\e_artist.css">
+        <link rel="stylesheet" href="../css/e_artist.css">
         <script src="//code.jquery.com/jquery-latest.js"></script>
         <title>구매전 안내</title>
     </head>
--- HTML/notice.php
+++ HTML/notice.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="..\CSS\notice.css">
+        <link rel="stylesheet" href="../css/notice.css">
         <script src="//code.jquery.com/jquery-latest.js"></script>
         <title>공지사항</title>
     </head>
